2967a9aa968fb245ff19c822966bf3cdb9af6d70
Khr178 <115107723+Khr178@users.noreply.github.com>
2022-11-09 19:57:47 +0200

    unit test

---
+++ юніт тест 5.3.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лабаа 5.3/лабаа 5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест53
+{
+	TEST_CLASS(юніттест53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
