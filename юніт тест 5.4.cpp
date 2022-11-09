8184a64a9a105cbd1a931d09f19dd3f3ed26a04d
Khr178 <115107723+Khr178@users.noreply.github.com>
2022-11-09 21:58:07 +0200

    unit test

---
+++ юніт тест 5.4.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 5.4/лаба 5.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест54
+{
+	TEST_CLASS(юніттест54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int N = 3;
+			int K = 2;
+
+			double res = P0(N,0);
+			
+
+			double expected = 0;
+
+
+			Assert::AreEqual(res, expected);
+		}
+	};
+}
