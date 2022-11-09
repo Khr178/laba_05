700cdd264ab176725291670b05ea4f1d5941356e
Khr178 <115107723+Khr178@users.noreply.github.com>
2022-11-09 17:39:49 +0200

    first commit

---
+++ лабораторна 5.1.cpp
@@ -0,0 +1,30 @@
+﻿// лабораторна 5.1.cpp 
+
+// Lab_05.1.cpp 
+// < Копейка Христина > 
+// Лабораторна робота № 5.1 
+// "Функції, що містять арифметичний вираз" 
+// Варіант 8 
+
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+
+using namespace std;
+double g(const double a, const double b); // прототип 
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+
+	double c = (g(s, t + 1) * (s, t + 1)) + (g(t, s + 1) * (t, s + 1)) / (1 + (g((s + t), (s * t)))) * (g((s + t), (s * t)));
+	cout << "с = " << c << endl;
+	return 0;
+}
+
+double g(const double a, const double b) // визначення 
+{
+	return ((sin(a * b)) / ((a * a) + (b * b)));
+};
