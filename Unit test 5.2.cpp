8ab85dd5683adc45994a73a24f2358090d2fa385
Khr178 <115107723+Khr178@users.noreply.github.com>
2022-11-09 18:14:43 +0200

    unit test

---
+++ Unit test 5.2.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <cassert>
+#include "../лаба 5.2/лаба 5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Unittest52
+{
+	TEST_CLASS(Unittest52)
+	{
+	public:
+		
+		TEST_METHOD(TestAFunction)
+		{
+			
+		}
+	};
+}
